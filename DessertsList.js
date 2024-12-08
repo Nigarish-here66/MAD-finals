647830f8b307927cb9ade3f6b09333a04db2d59a
Nagarash Fateh <121384652+Nigarish-here66@users.noreply.github.com>
2024-12-08 18:14:31 +0500

    auto-commit

--- DessertsList.js
+++ DessertsList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function DessertsList({ data }) {
-  // Filter desserts with less than 500 calories and sort them by calories
   const lowCalorieDesserts = data
     .filter(dessert => dessert.calories < 500)
     .sort((a, b) => a.calories - b.calories);
